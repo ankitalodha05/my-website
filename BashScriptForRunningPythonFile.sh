f803f508b1eef84f854dc17be3ca4835b5fcb610
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2024-10-09 23:35:26 +0530

    Update BashScriptForRunningPythonFile.sh

--- BashScriptForRunningPythonFile.sh
+++ BashScriptForRunningPythonFile.sh
@@ -2,4 +2,5 @@
 # Activate the virtual environment
 source /path/to/script/venv/bin/activate
 # Run the Python script
-python3 /root/autoCode.py
+# python3 /root/autoCode.py
+python3 /root/PythonAutoCICD.py
